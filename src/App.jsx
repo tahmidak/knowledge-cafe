fd5c19f6d0932981fcea9a390cc93b17e225f5d9
TahmidAhmedKhan <TahmidAhmedKhan@users.noreply.github.com>
2025-08-16 02:09:25 +0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+    <Header>
+      
+    </Header>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,10 @@
+const header = () => {
+    return (
+        <div>
+            <div className='text-4xl font-bold'>Knowledge Cafe</div>
+        </div>
+    );
+};
+
+export default header;
+
